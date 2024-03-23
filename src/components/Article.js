af3ea4c0d218f091c2832d9b54750c79feeb50f3
justinfernz <jdotfernz@gmail.com>
2024-03-23 12:51:05 -0400

    Update dependencies

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,7 @@ import React from "react";
 function Article(props) {
     return (
         <article >
-            <a href={ "https://reddit.com" + props.article.permalink } target="_blank" > 
+            <a href={ "https://reddit.com" + props.article.permalink } target="_blank" rel="noreferrer"> 
                 <h3 data-testid="titles">{props.article.title}</h3>
             </a>
         </article>
